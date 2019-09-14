d0d7e54e55b5918a08a49911a98edace6e7e7d5e
Atsushi Sakai <asakai.amsl+github@gmail.com>
2019-09-14 21:51:11 +0900

    update LEDControl.py

--- GPIOSamples/LEDControl.py
+++ GPIOSamples/LEDControl.py
@@ -4,10 +4,15 @@ from time import sleep
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(25, GPIO.OUT)
 
-while True:
-    print("High")
-    GPIO.output(25, GPIO.HIGH)
-    sleep(0.5)
-    print("Low")
-    GPIO.output(25, GPIO.LOW)
-    sleep(0.5)
+try:
+    while True:
+        print("High")
+        GPIO.output(25, GPIO.HIGH)
+        sleep(0.5)
+        print("Low")
+        GPIO.output(25, GPIO.LOW)
+        sleep(0.5)
+except KeyboardInterrupt:
+    pass
+
+GPIO.cleanup()
